89b31ccf3fa03e99e547d048c776417725316ec8
Youssef Aboukir <yaboukir@e3r3p11.1337.ma>
2025-08-08 11:30:40 +0100

    fixing

--- CPP00/ex01/Phonebook.cpp
+++ CPP00/ex01/Phonebook.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Phonebook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: onevil_x <onevil_x@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yaboukir <yaboukir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 00:15:44 by onevil_x          #+#    #+#             */
-/*   Updated: 2025/07/19 00:29:19 by onevil_x         ###   ########.fr       */
+/*   Updated: 2025/08/08 11:29:59 by yaboukir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 PhoneBook::PhoneBook() : contactCount(0) {}
 
-void PhoneBook::addContact() {
+void PhoneBook::addContact() 
+{
     Contact newContact;
     std::string input;
 
@@ -43,7 +44,8 @@ void PhoneBook::addContact() {
 	std::cout << "Contact added successfully!" << std::endl;
 }
 
-static std::string formatField(const std::string &str) {
+static std::string formatField(const std::string &str) 
+{
 	if (str.length() > 10)
 		return str.substr(0, 9) + ".";
 	else if (str.length() < 10)
@@ -51,7 +53,7 @@ static std::string formatField(const std::string &str) {
 	return str;
 }
 
-void PhoneBook::searchContact() const {
+void PhoneBook::searchContact() const {	
 	if (contactCount == 0) 
 	{
 		std::cout << "No Contacts in the PhoneBook" << std::endl;
--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: onevil_x <onevil_x@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yaboukir <yaboukir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 02:09:17 by onevil_x          #+#    #+#             */
-/*   Updated: 2025/07/19 01:38:25 by onevil_x         ###   ########.fr       */
+/*   Updated: 2025/08/08 11:15:12 by yaboukir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,9 @@ int main()
         std::cout << "___________________________________" << std::endl;
         std::cout << "Enter a command {ADD, SEARCH, EXIT}" << std::endl;
         std::getline(std::cin, cmd);
-        
+
+        if (std::cin.eof())
+            return 0;
         if (cmd == "ADD")
         phoneBook.addContact();
         else if (cmd == "SEARCH")
